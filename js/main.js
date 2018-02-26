b5bbd8ec43f0b4345fc6f97f7ea7786d5af309dd
Mac <mac_radinoff@msn.com>
2018-02-26 21:03:41 +1100

    removed cheating

--- js/main.js
+++ js/main.js
@@ -19,11 +19,11 @@ var cards = [
 var cardsInPlay = [];
 
  checkForMatch = function(){
-	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) { 
+	if (cardsInPlay.length === 4 && cardsInPlay[0] === cardsInPlay[2]) { 
 		setTimeout(function(){alert("You found a match!") ? " ":location.reload();}, 500)
 		
 	}
-	else if (cardsInPlay.length === 2){
+	else if (cardsInPlay.length === 4){
 		setTimeout(function(){alert("Sorry, try again.") ? " ": location.reload();}, 500);
 	}
 }
@@ -31,9 +31,16 @@ var flipCard = function(){
 	var cardId = this.getAttribute('data-id')
 	console.log("User flipped " + cards[cardId].rank + " of " +cards[cardId].suit + " "+ cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank); 
+	cardsInPlay.push(cards[cardId].suit);
 	this.setAttribute('src',cards[cardId].cardImage);
+	if (cardsInPlay.length === 4 && cardsInPlay[1] === cardsInPlay[3] && cardsInPlay[0] === cardsInPlay[2]) {
+		cardsInPlay.pop();
+		cardsInPlay.pop();
+	}
+	else{
 	checkForMatch ();
-}
+	}
+}	
 var createBoard = function () {
 	for (var i = 0; i < cards.length; i++){
 		var j = Math.floor(Math.random()*(i+1));
